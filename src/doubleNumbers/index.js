181108d359a7198ac6436afa5593506c683198cb
Vitaliy <ivahiv99@gmail.com>
2018-10-31 19:49:32 +0200

    done

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,18 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+  const isMirror = str =>{
+    const halfLength = str.length / 2;
+    const first = str.slice(0 , halfLength);
+    const second = str.slice(halfLength , str.length);
+    return first === second ? true : false;
+  };
+
+  const stringNum = num.toString();
+
+  if (stringNum.length % 2 === 0 && isMirror(stringNum)){
+    return num;
+  } else {
+    return num * 2;
+  }
+
 };
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,27 @@
 /* eslint-disable-next-line */
 export const sortByWeight = arr => {
-  // your logic here...
+  const sumOfElement = numb =>{
+    const arr = numb.toString().split('').map(Number);
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum ;
+  };
+  let min;
+  let buff = 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    min = arr[i];
+    for (let j = i + 1;  j < arr.length; j++) {
+      if (sumOfElement(min) > sumOfElement(arr[j])){
+        min = arr[j];
+        buff = arr[j];
+        arr[j] = arr[i];
+        arr[i] = buff;
+      }
+    }
+  }
+  return arr;
 };
+
+
